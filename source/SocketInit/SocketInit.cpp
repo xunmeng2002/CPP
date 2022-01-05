cb4b8904ec4e820c7ae1da4c4bf27cdc38421159
紫云 <150315803@qq.com>
2022-01-05 17:14:28 +0800

    a

--- source/SocketInit/SocketInit.cpp
+++ source/SocketInit/SocketInit.cpp
@@ -3,23 +3,33 @@
 
 static SocketInit _SOCKET_INIT;
 
+using namespace std;
+
 SocketInit::SocketInit()
 {
+#ifdef WINDOWS
+	cout << "ON WINDOWS" << endl;
 	WSADATA initData;
 	initData.wVersion = 0;
 	initData.wHighVersion = 2;
-	strcpy(initData.szDescription, "");
-	strcpy(initData.szSystemStatus, "");
+	memset(initData.szDescription, 0, sizeof(initData.szDescription));
+	memset(initData.szSystemStatus, 0, sizeof(initData.szSystemStatus));
 	initData.iMaxSockets = 100;
 	initData.lpVendorInfo = nullptr;
 	if (WSAStartup(2, &initData) != 0)
 	{
 		std::cout << "SocketInit Failed!" << std::endl;
 	}
+#endif
+#ifdef LINUX
+	cout << "ON LINUX" << endl;
+#endif
 }
 
 SocketInit::~SocketInit()
 {
+#ifdef WINDOWS
 	WSACleanup();
+#endif
 }
 
--- source/SocketInit/SocketInit.h
+++ source/SocketInit/SocketInit.h
@@ -1,8 +1,12 @@
 #pragma once
-#include <WinSock2.h>
-#include <MSWSock.h>
+#ifdef WINDOWS
+#include <Ws2tcpip.h>
 #pragma comment(lib,"ws2_32.lib")
-
+#endif // WINDOWS
+#ifdef LINUX
+#include <arpa/inet.h>
+#include <netdb.h>
+#endif // LINUX
 
 
 class SocketInit
